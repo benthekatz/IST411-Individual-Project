732ce0b435474096f93ae519db842e6fda7122c5
benthekatz <benthekatz@gmail.com>
2018-04-12 12:26:50 -0400

    Firebase DB Storage of Users

--- SpringMVC/individual_project/target/classes/static/js/login.js
+++ SpringMVC/individual_project/target/classes/static/js/login.js
@@ -4,6 +4,11 @@ var modal = document.getElementById("loginModal");
 var close = document.getElementsByClassName("close")[0];
 var error = document.getElementById("error");
 
+//firebase authentication
+var provider = new firebase.auth.GoogleAuthProvider();
+
+//firebase database instance
+var database = firebase.database();
 
 var initClient = function () {
     gapi.load('auth2', function () {
@@ -25,17 +30,49 @@ var initClient = function () {
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('ID: ' + profile.getId());
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    console.log('Email: ' + profile.getEmail());
 
-    var id_token = googleUser.getAuthResponse().id_token;
-    
+    console.log('Google Auth Response', googleUser);
+    var unsubscribe = firebase.auth().onAuthStateChanged(function (firebaseUser) {
+        unsubscribe();
+        
+        if (!isUserEqual(googleUser, firebaseUser)) {
+            var credential = firebase.auth.GoogleAuthProvider.credential(
+                    googleUser.getAuthResponse().id_token);
+            firebase.auth().signInWithCredential(credential).catch(function (error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+                console.log(errorCode, errorMessage, email, credential);
+            });
+        } else {
+            console.log('User already signed-in Firebase.');
+        }
+    });
+
+    writeUserData(profile.getId(), profile.getName(), profile.getEmail(), profile.getImageUrl());
 
     loginStatus = true;
     $(".collapse").collapse("show");
     modal.style.display = "none"
+}
+
+function isUserEqual(googleUser, firebaseUser) {
+    if (firebaseUser) {
+        var providerData = firebaseUser.providerData;
+        for (var i = 0; i < providerData.length; i++) {
+            if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+                    providerData[i].uid === googleUser.getBasicProfile().getId()) {
+                // We don't need to reauth the Firebase connection.
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 var onFailure = function (error) {
@@ -59,5 +96,13 @@ $("#canvas").mousedown(function (e) {
 
 close.onclick = function () {
     modal.style.display = "none";
+}
+
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
 }
 
